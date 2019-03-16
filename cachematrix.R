1d7534dfce8fbb02070164730aeb08e6e6a16d59
rommel santiago <cocosanti@gmail.com>
2019-03-15 18:29:04 -0600

    Adding body of makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create a mechanism to cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## The function creates a special matrix to cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) iv <<- inverse
+  getinverse <- function() inv
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the special matrix computed by the function above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  if(nrow(data) != ncol(data)){return(message("Matrix must be square"))}
+  if(det(data) != 0  ){
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv  
+  }else{
+    message("Cannot compute inverse")
+  }
+  
 }
